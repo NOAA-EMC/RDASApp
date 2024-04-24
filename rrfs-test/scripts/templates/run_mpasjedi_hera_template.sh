e99b1a1dbbc2b41df7cb95484ace1e474f27607f
delippi <31075260+delippi@users.noreply.github.com>
2024-04-24 14:48:18 -0400

    Add scripts for setting up an experiment from ctest data and more (#25)
    add scripts to help a user set up and
    run their own test case from the ctest data.

---
+++ rrfs-test/scripts/templates/run_mpasjedi_hera_template.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+#SBATCH --account=fv3-cam
+#SBATCH --qos=debug
+#SBATCH --ntasks=36
+#SBATCH -t 00:30:00
+#SBATCH --job-name=mpasjedi_test
+#SBATCH -o jedi.log
+#SBATCH --open-mode=truncate
+#SBATCH --cpus-per-task 4 --exclusive
+
+. /apps/lmod/lmod/init/sh
+set +x
+
+module purge
+
+module use @YOUR_PATH_TO_RDASAPP@/modulefiles
+module load RDAS/hera.intel
+
+module list
+
+export OOPS_TRACE=1
+export OMP_NUM_THREADS=1
+
+ulimit -s unlimited
+ulimit -v unlimited
+ulimit -a
+
+inputfile=$1
+if [[ $inputfile == "" ]]; then
+  inputfile=./testinput/rrfs_mpasjedi_2022052619_Ens3Dvar.yaml
+fi
+
+jedibin="@YOUR_PATH_TO_RDASAPP@/build/bin"
+# Run JEDI - currently cannot change processor count
+srun -l -n 36 $jedibin/mpasjedi_variational.x ./$inputfile out.log
+
