c4b32eed797069f88a0877d9289328623720e9f9
Donald.E.Lippi <donald.e.lippi@noaa.gov>
2024-08-21 16:35:40 +0000

    Added aircraft yamls, updated the gen_yaml script, renamed mesonet yamls.

--- rrfs-test/validated_yamls/gen_yaml.sh
+++ rrfs-test/validated_yamls/gen_yaml.sh
@@ -2,16 +2,44 @@
 
 #use this script to combine the basic config yaml with an obtype config yaml.
 basic_config="fv3jedi_hyb3denvar.yaml"
-obtype_config="msonet_airTemperature_obtype188.yaml"
+obtype_configs="
+aircft_airTemperature_130.yaml
+aircft_airTemperature_131.yaml
+aircft_airTemperature_133.yaml
+aircft_airTemperature_134.yaml
+aircft_airTemperature_135.yaml
 
-# Don't edit below this line.
+aircft_specificHumidity_133.yaml
+aircft_specificHumidity_134.yaml
+
+aircft_uv_230.yaml
+aircft_uv_231.yaml
+aircft_uv_233.yaml
+aircft_uv_234.yaml
+aircft_uv_235.yaml
+
+msonet_airTemperature_188.yaml
+msonet_specificHumidity_188.yaml
+msonet_stationPressure_188.yaml
+msonet_uv_288.yaml
+"
+
+obtype="conv.yaml"
+
+temp_config="combined.yaml"
+rm -f $temp_config
+echo "catting the following yamls:"
+for obtype_config in $obtype_configs; do
+    echo "   $obtype_config"
+    cat ./templates/obtype_config/$obtype_config >> ./$temp_config
+done
 
 # copy the basic configuration yaml into the new yaml
-cp -p templates/basic_config/$basic_config ./$obtype_config
+cp -p templates/basic_config/$basic_config ./${obtype}
 
 # use the stread editor to replace the instance of @OBSERVATION@ placeholder
 # in the basic configuration file with the contents of the obtype config yaml.
 sed -i '/@OBSERVATIONS@/{
-        r templates/obtype_config/'"${obtype_config}"'
+        r ./'"${temp_config}"'
         d
-}' ./$obtype_config
+}' ./${obtype}
