97afb4b98277bfe7b48fbfbc4d1026ea8de4ee22
Guoqing Ge <guoqing.ge@noaa.gov>
2024-09-03 09:41:33 -0600

    streamline the module load process and the rrfs-test process (#141)
    Simplify the module load process and the rrfs-test step so that each can
    be done with one simple command.

---
+++ ush/run_rrfs_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+export SLURM_ACCOUNT=${1}
+
+if [[ "${1}" == "" ]]; then
+  echo "Usage: ${0}  account_name"
+  exit 1
+fi
+
+#
+ushdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source ${ushdir}/load_rdas.sh
+
+set -x
+cd ${ushdir}/../build/rrfs-test
+pwd
+ctest # or ctest -VV for verbose outputs
