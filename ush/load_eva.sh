7025b731e74ec222da3cc922b6958cc8ecb999dc
guoqing.ge <Guoqing.Ge@noaa.gov>
2024-10-14 21:38:22 -0400

    cosmetic changes

--- ush/load_eva.sh
+++ ush/load_eva.sh
@@ -12,7 +12,7 @@ source ${ushdir}/detect_machine.sh
 
 module purge
 module use ${ushdir}/../modulefiles
-if [[ "$MACHINE_ID" == "gaea" ]]; then
+if [[ "${MACHINE_ID}" == "gaea" ]]; then
   if [[ -d /gpfs/f5 ]]; then
     module load EVA/${MACHINE_ID}C5
   elif [[ -d /gpfs/f6 ]]; then
