429d4bc1a35eec77fe01dd8efd28dbc849d357ae
guoqing.ge <Guoqing.Ge@noaa.gov>
2024-10-14 21:36:14 -0400

    minor changes

--- ush/load_eva.sh
+++ ush/load_eva.sh
@@ -12,10 +12,14 @@ source ${ushdir}/detect_machine.sh
 
 module purge
 module use ${ushdir}/../modulefiles
-if [[ -d /gpfs/f5 ]]; then
-  module load EVA/${MACHINE_ID}C5
-elif [[ -d /gpfs/f6 ]]; then
-  module load EVA/${MACHINE_ID}C6
+if [[ "$MACHINE_ID" == "gaea" ]]; then
+  if [[ -d /gpfs/f5 ]]; then
+    module load EVA/${MACHINE_ID}C5
+  elif [[ -d /gpfs/f6 ]]; then
+    module load EVA/${MACHINE_ID}C6
+  else
+    echo "not supported gaea cluster: ${MACHINE_ID}"
+  fi
 else
   module load EVA/${MACHINE_ID}
 fi
