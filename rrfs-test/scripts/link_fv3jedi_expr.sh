03a7598453ebf7244fb3ca6dd8436a6d10618d9a
SamuelDegelia-NOAA <Samuel.Degelia@noaa.gov>
2024-11-20 03:39:32 +0000

    Fix linking script for new yaml files

--- rrfs-test/scripts/link_fv3jedi_expr.sh
+++ rrfs-test/scripts/link_fv3jedi_expr.sh
@@ -15,8 +15,8 @@ cd ${expdir}
 echo "expdir is at: ${expdir}"
 
 ${RDASApp}/ush/init.sh
-cp ${RDASApp}/rrfs-test/testinput/rrfs_fv3jedi_hyb_2022052619.yaml .
-cp ${RDASApp}/rrfs-test/testinput/rrfs_fv3jedi_letkf_2022052619.yaml . 
+cp ${RDASApp}/rrfs-test/testinput/rrfs_fv3jedi_2024052700_hyb.yaml .
+cp ${RDASApp}/rrfs-test/testinput/rrfs_fv3jedi_2024052700_letkf.yaml . 
 sed -e "s#@RDASApp@#${RDASApp}#" ${RDASApp}/rrfs-test/scripts/templates/fv3jedi_expr/run_bump.sh > run_bump.sh
 sed -e "s#@RDASApp@#${RDASApp}#" ${RDASApp}/rrfs-test/scripts/templates/fv3jedi_expr/run_jedi.sh > run_jedi.sh
 cp ${RDASApp}/rrfs-test/ush/colormap.py . 
