5bfc9db40fd924dea83c465161b704dd68b7d081
SamuelDegelia-NOAA <Samuel.Degelia@noaa.gov>
2025-02-07 20:38:07 +0000

    Fix cd paths in build.sh

--- build.sh
+++ build.sh
@@ -143,20 +143,20 @@ fi
 if [[ $DYCORE == 'MPAS' || $DYCORE == 'FV3andMPAS' ]]; then
   # Link in case data
   echo "Linking in test data and creating ctest yamls for MPAS-JEDI case"
-  $dir_root/rrfs-test/scripts/link_mpasjedi_expr.sh
   cd $dir_root/rrfs-test/validated_yamls
   ./gen_yaml_mpasjedi_ctest.sh
   cd ${BUILD_DIR}
+  $dir_root/rrfs-test/scripts/link_mpasjedi_expr.sh
 fi
 
 # Link in test data for experiments and create ctest yamls: FV3-JEDI
 if [[ $DYCORE == 'FV3' || $DYCORE == 'FV3andMPAS' ]]; then
   # Link in case data
   echo "Linking in test data and creating ctest yamls for FV3-JEDI case"
-  $dir_root/rrfs-test/scripts/link_fv3jedi_expr.sh
   cd $dir_root/rrfs-test/validated_yamls
   ./gen_yaml_fv3jedi_ctest.sh
   cd ${BUILD_DIR}
+  $dir_root/rrfs-test/scripts/link_fv3jedi_expr.sh
 fi
 
 CMAKE_OPTS+=" -DMPIEXEC_MAX_NUMPROCS:STRING=120 -DBUILD_SUPER_EXE=$BUILD_SUPER_EXE"
@@ -167,8 +167,6 @@ cmake \
   ${CMAKE_OPTS:-} \
   $dir_root/bundle
 set +x
-
-exit #@@@ TODO: REMOVE ME 
 
 # Build
 echo "Building ..."
