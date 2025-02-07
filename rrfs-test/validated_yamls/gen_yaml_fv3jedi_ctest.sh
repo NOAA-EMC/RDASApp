75a2b0c23e19093a2764e4f8f6b059872133876e
SamuelDegelia-NOAA <Samuel.Degelia@noaa.gov>
2025-02-07 19:50:49 +0000

    Modify gen_yaml_ctest scripts to be much easier to add new ob types

--- rrfs-test/validated_yamls/gen_yaml_fv3jedi_ctest.sh
+++ rrfs-test/validated_yamls/gen_yaml_fv3jedi_ctest.sh
@@ -1,63 +1,74 @@
 #!/bin/bash
 
-# Define the basic configuration YAMLs
-basic_configs=(
-    "fv3jedi_en3dvar.yaml"
-    "fv3jedi_getkf_observer.yaml"
-    "fv3jedi_getkf_solver.yaml"
-)
+# Function to add key-value pairs to a dictionary and maintain order
+add_to_dict() {
+    local -n dict_ref=$1
+    local -n keys=$2
+    local key=$3
+    local value=$4
+    dict_ref["$key"]="$value"
+    keys+=("$key")
+}
 
-# CTest yaml outputs
-ctest_names=(
-    "rrfs_fv3jedi_2024052700_Ens3Dvar.yaml"
-    "rrfs_fv3jedi_2024052700_getkf_observer.yaml"
-    "rrfs_fv3jedi_2024052700_getkf_solver.yaml"
-)
+###############################################
+### Edit this section if adding more ctests ###
+###############################################
 
-# Define the aircar observation type configs as an array
-aircar_obtype_configs=(
-    "aircar_airTemperature_133.yaml"
-    "aircar_uv_233.yaml"
-    "aircar_specificHumidity_133.yaml"
-)
+# Dictionary to define the basic configuration yamls and the ctest output names
+declare -A ctest_configs
+keys_ctest=()
+add_to_dict ctest_configs keys_ctest "fv3jedi_en3dvar.yaml"        "rrfs_fv3jedi_2024052700_Ens3Dvar.yaml" 
+add_to_dict ctest_configs keys_ctest "fv3jedi_getkf_observer.yaml" "rrfs_fv3jedi_2024052700_getkf_observer.yaml" 
+add_to_dict ctest_configs keys_ctest "fv3jedi_getkf_solver.yaml"   "rrfs_fv3jedi_2024052700_getkf_solver.yaml" 
 
-# Define the aircft observation type configs as an array
-aircft_obtype_configs=(
-    "aircft_airTemperature_130.yaml"
-    "aircft_airTemperature_131.yaml"
-    "aircft_airTemperature_134.yaml"
-    "aircft_airTemperature_135.yaml"
-    "aircft_specificHumidity_134.yaml"
-    "aircft_uv_230.yaml"
-    "aircft_uv_231.yaml"
-    "aircft_uv_234.yaml"
-    "aircft_uv_235.yaml"
-)
 
-# Define msonet observation type configs as an array
-msonet_obtype_configs=(
-    "msonet_airTemperature_188.yaml"
-    "msonet_specificHumidity_188.yaml"
-    "msonet_stationPressure_188.yaml"
-    "msonet_winds_288.yaml"
-)
+########################################################
+### Edit this section if adding more obs-space yamls ###
+########################################################
 
-# Define ATMS observation type configs as an array
-atms_obtype_configs=(
-    "atms_npp_qc_bc.yaml"
-)
-# Define AMSUA observation type configs as an array
-amsua_obtype_configs=(
-    "amsua_n19.yaml"
-)
+# Dictionary to define the ob-space yamls to be merged in along with the obs file used for them 
+declare -A obtype_configs
+keys_obtype=()
+add_to_dict obtype_configs keys_obtype "aircar_airTemperature_133.yaml"    "data/obs/ioda_aircar_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircar_specificHumidity_133.yaml"  "data/obs/ioda_aircar_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircar_winds_233.yaml"             "data/obs/ioda_aircar_dc"
+add_to_dict obtype_configs keys_obtype "aircft_airTemperature_130.yaml"    "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_airTemperature_131.yaml"    "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_airTemperature_134.yaml"    "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_airTemperature_135.yaml"    "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_specificHumidity_134.yaml"  "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_winds_230.yaml"             "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_winds_231.yaml"             "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_winds_234.yaml"             "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_winds_235.yaml"             "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "msonet_airTemperature_188.yaml"    "data/obs/ioda_msonet_dc.nc"
+add_to_dict obtype_configs keys_obtype "msonet_specificHumidity_188.yaml"  "data/obs/ioda_msonet_dc.nc"
+add_to_dict obtype_configs keys_obtype "msonet_stationPressure_188.yaml"   "data/obs/ioda_msonet_dc.nc"
+add_to_dict obtype_configs keys_obtype "msonet_winds_288.yaml"             "data/obs/ioda_msonet_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpsfc_airTemperature_187.yaml"    "data/obs/ioda_adpsfc_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpsfc_specificHumidity_187.yaml"  "data/obs/ioda_adpsfc_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpsfc_stationPressure_187.yaml"   "data/obs/ioda_adpsfc_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpsfc_winds_287.yaml"             "data/obs/ioda_adpsfc_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpupa_airTemperature_120.yaml"    "data/obs/ioda_adpupa_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpupa_specificHumidity_120.yaml"  "data/obs/ioda_adpupa_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpupa_winds_220.yaml"             "data/obs/ioda_adpupa_dc.nc"
+add_to_dict obtype_configs keys_obtype "vadwnd_winds_224.yaml"             "data/obs/ioda_vadwnd_dc.nc" 
+add_to_dict obtype_configs keys_obtype "atms_npp_qc_bc.yaml"               "data/obs/atms_npp_obs_2024052700_dc.nc"
+#add_to_dict obtype_configs keys_obtype "atms_n20.yaml"                     "data/obs/ioda_atms_n20.nc" # file missing?
+add_to_dict obtype_configs keys_obtype "amsua_n19.yaml"                    "data/obs/amsua_n19_obs.2024052700_dc.nc" 
+
+
+#############################
+### Begin executable code ###
+#############################
 
 # Function to concatenate all obtypes into one file
 process_obtypes() {
-    local ctest="$1"
-    local obtype_configs=("${!2}")  # Accept array as input
-    local obs_filename="$3"
-    local temp_yaml="$4"
-    
+    local -n obtype_config_in=$1
+    local -n keys_obtype_in=$2
+    local ctest=$3
+    local temp_yaml=$4
+
     # Determine the ctest type to select the observation distribution
     if [[ $ctest == *"solver"* ]]; then
         distribution="Halo"
@@ -66,7 +77,9 @@ process_obtypes() {
     fi
 
     echo "Appending the following yamls:"
-    for obtype_config in "${obtype_configs[@]}"; do
+    for key in "${keys_obtype_in[@]}"; do 
+        obtype_config=$key
+        obs_filename=${obtype_config_in[$key]}
         echo "   $obtype_config"
         cat ./templates/obtype_config/$obtype_config >> ./$temp_yaml
 
@@ -89,21 +102,17 @@ process_obtypes() {
 
 # Loop over basic config yamls
 iconfig=0
-for basic_config in "${basic_configs[@]}"; do
+for basic_config in "${!ctest_configs[@]}"; do
 
   # Create the super yaml (conv.yaml)
-  conv_yaml="${ctest_names[$iconfig]}"
+  conv_yaml="${ctest_configs[$basic_config]}"
   temp_yaml="temp.yaml"
 
   rm -f $conv_yaml  # Remove any existing file
   rm -f $temp_yaml  # Remove any existing file
 
   # Concatenate all obtypes into the super yaml
-  process_obtypes "${ctest_names[$iconfig]}" "aircar_obtype_configs[@]" "Data/obs/ioda_aircar_dc.nc"             "$temp_yaml"
-  #process_obtypes "${ctest_names[$iconfig]}" "aircft_obtype_configs[@]" "Data/obs/ioda_aircft_dc.nc"             "$temp_yaml"
-  process_obtypes "${ctest_names[$iconfig]}" "msonet_obtype_configs[@]" "Data/obs/ioda_msonet_dc.nc"             "$temp_yaml"
-  process_obtypes "${ctest_names[$iconfig]}" "atms_obtype_configs[@]"   "Data/obs/atms_npp_obs_2024052700_dc.nc" "$temp_yaml"
-  #process_obtypes "${ctest_names[$iconfig]}" "amsua_obtype_configs[@]"  "Data/obs/ioda_amsua_n19_dc.nc"      "$temp_yaml"
+  process_obtypes obtype_configs keys_obtype "${conv_yaml}" "$temp_yaml"
 
   # Copy the basic configuration yaml into the super yaml
   cp -p templates/basic_config/$basic_config ./$conv_yaml
@@ -115,11 +124,10 @@ for basic_config in "${basic_configs[@]}"; do
   }' ./$conv_yaml
 
   # Replace the @OBSFILE@ placeholder with a dummy filename (can customize as needed)
-  sed -i "s#@OBSFILE@#Data/obs/combined_obs_file.nc#" ./$conv_yaml
-
-  echo "Super YAML created in ${conv_yaml}"
+  sed -i "s#@OBSFILE@#data/obs/combined_obs_file.nc#" ./$conv_yaml
 
   # Move to testinput and remove the old temporary yaml
+  echo "Super YAML created in ${conv_yaml}"
   mv $conv_yaml ../testinput/$conv_yaml
   rm -f $temp_yaml
 
--- rrfs-test/validated_yamls/gen_yaml_mpasjedi_ctest.sh
+++ rrfs-test/validated_yamls/gen_yaml_mpasjedi_ctest.sh
@@ -1,62 +1,73 @@
 #!/bin/bash
 
-# Define the basic configuration YAMLs
-basic_configs=(
-    "mpasjedi_en3dvar.yaml"
-    "mpasjedi_getkf_observer.yaml"
-    "mpasjedi_getkf_solver.yaml"
-)
+# Function to add key-value pairs to a dictionary and maintain order
+add_to_dict() {
+    local -n dict_ref=$1
+    local -n keys=$2
+    local key=$3
+    local value=$4
+    dict_ref["$key"]="$value"
+    keys+=("$key")
+}
 
-# CTest yaml outputs 
-ctest_names=(
-    "rrfs_mpasjedi_2024052700_Ens3Dvar.yaml"
-    "rrfs_mpasjedi_2024052700_getkf_observer.yaml"
-    "rrfs_mpasjedi_2024052700_getkf_solver.yaml"
-)
-    
-# Define the aircar observation type configs as an array
-aircar_obtype_configs=(
-    "aircar_airTemperature_133.yaml"
-    "aircar_uv_233.yaml"
-    "aircar_specificHumidity_133.yaml"
-)
+###############################################
+### Edit this section if adding more ctests ###
+###############################################
 
-# Define the aircft observation type configs as an array
-aircft_obtype_configs=(
-    "aircft_airTemperature_130.yaml"
-    "aircft_airTemperature_131.yaml"
-    "aircft_airTemperature_134.yaml"
-    "aircft_airTemperature_135.yaml"
-    "aircft_specificHumidity_134.yaml"
-    "aircft_uv_230.yaml"
-    "aircft_uv_231.yaml"
-    "aircft_uv_234.yaml"
-    "aircft_uv_235.yaml"
-)
+# Dictionary to define the basic configuration yamls and the ctest output names
+declare -A ctest_configs
+keys_ctest=()
+add_to_dict ctest_configs keys_ctest "mpasjedi_en3dvar.yaml"        "rrfs_mpasjedi_2024052700_Ens3Dvar.yaml" 
+add_to_dict ctest_configs keys_ctest "mpasjedi_getkf_observer.yaml" "rrfs_mpasjedi_2024052700_getkf_observer.yaml" 
+add_to_dict ctest_configs keys_ctest "mpasjedi_getkf_solver.yaml"   "rrfs_mpasjedi_2024052700_getkf_solver.yaml" 
 
-# Define msonet observation type configs as an array
-msonet_obtype_configs=(
-    "msonet_airTemperature_188.yaml"
-    "msonet_specificHumidity_188.yaml"
-    "msonet_stationPressure_188.yaml"
-    "msonet_wind_288.yaml"
-)
 
-# Define ATMS observation type configs as an array
-atms_obtype_configs=(
-    "atms_npp_qc_bc.yaml"
-)
-# Define AMSUA observation type configs as an array
-amsua_obtype_configs=(
-    "amsua_n19.yaml"
-)
+########################################################
+### Edit this section if adding more obs-space yamls ###
+########################################################
+
+# Dictionary to define the ob-space yamls to be merged in along with the obs file used for them 
+declare -A obtype_configs
+keys_obtype=()
+add_to_dict obtype_configs keys_obtype "aircar_airTemperature_133.yaml"    "data/obs/ioda_aircar_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircar_specificHumidity_133.yaml"  "data/obs/ioda_aircar_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircar_winds_233.yaml"             "data/obs/ioda_aircar_dc"
+add_to_dict obtype_configs keys_obtype "aircft_airTemperature_130.yaml"    "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_airTemperature_131.yaml"    "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_airTemperature_134.yaml"    "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_airTemperature_135.yaml"    "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_specificHumidity_134.yaml"  "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_winds_230.yaml"             "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_winds_231.yaml"             "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_winds_234.yaml"             "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "aircft_winds_235.yaml"             "data/obs/ioda_aircft_dc.nc"
+add_to_dict obtype_configs keys_obtype "msonet_airTemperature_188.yaml"    "data/obs/ioda_msonet_dc.nc"
+add_to_dict obtype_configs keys_obtype "msonet_specificHumidity_188.yaml"  "data/obs/ioda_msonet_dc.nc"
+add_to_dict obtype_configs keys_obtype "msonet_stationPressure_188.yaml"   "data/obs/ioda_msonet_dc.nc"
+add_to_dict obtype_configs keys_obtype "msonet_winds_288.yaml"             "data/obs/ioda_msonet_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpsfc_airTemperature_187.yaml"    "data/obs/ioda_adpsfc_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpsfc_specificHumidity_187.yaml"  "data/obs/ioda_adpsfc_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpsfc_stationPressure_187.yaml"   "data/obs/ioda_adpsfc_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpsfc_winds_287.yaml"             "data/obs/ioda_adpsfc_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpupa_airTemperature_120.yaml"    "data/obs/ioda_adpupa_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpupa_specificHumidity_120.yaml"  "data/obs/ioda_adpupa_dc.nc"
+add_to_dict obtype_configs keys_obtype "adpupa_winds_220.yaml"             "data/obs/ioda_adpupa_dc.nc"
+add_to_dict obtype_configs keys_obtype "vadwnd_winds_224.yaml"             "data/obs/ioda_vadwnd_dc.nc" 
+add_to_dict obtype_configs keys_obtype "atms_npp_qc_bc.yaml"               "data/obs/atms_npp_obs_2024052700_dc.nc"
+#add_to_dict obtype_configs keys_obtype "atms_n20.yaml"                     "data/obs/ioda_atms_n20.nc" # file missing?
+add_to_dict obtype_configs keys_obtype "amsua_n19.yaml"                    "data/obs/amsua_n19_obs.2024052700_dc.nc" 
+
+
+#############################
+### Begin executable code ###
+#############################
 
 # Function to concatenate all obtypes into one file
 process_obtypes() {
-    local ctest="$1"
-    local obtype_configs=("${!2}")  # Accept array as input
-    local obs_filename="$3"
-    local temp_yaml="$4"
+    local -n obtype_config_in=$1
+    local -n keys_obtype_in=$2
+    local ctest=$3
+    local temp_yaml=$4
 
     # Determine the ctest type to select the observation distribution
     if [[ $ctest == *"solver"* ]]; then
@@ -66,18 +77,20 @@ process_obtypes() {
     fi
 
     echo "Appending the following yamls:"
-    for obtype_config in "${obtype_configs[@]}"; do
+    for key in "${keys_obtype_in[@]}"; do 
+        obtype_config=$key
+        obs_filename=${obtype_config_in[$key]}
         echo "   $obtype_config"
         cat ./templates/obtype_config/$obtype_config >> ./$temp_yaml
 
         # For EnKF solver ctests, replace obsfile path with output from corresponding observer ctest
-	if [[ $ctest == *"solver"* ]]; then 
-	   previous_path=`sed -n '/obsdataout/{n; n; n; s/^[[:space:]]\+//; p;}' ./templates/obtype_config/$obtype_config`
-   	   int_path=$(echo "$previous_path" | sed "s/obsfile: /..\/rundir-${ctest::-5}\//gI")
-	   new_path=$(echo "$int_path" | sed "s/solver/observer/gI")
-	   obs_filename=${new_path}
+        if [[ $ctest == *"solver"* ]]; then
+           previous_path=`sed -n '/obsdataout/{n; n; n; s/^[[:space:]]\+//; p;}' ./templates/obtype_config/$obtype_config`
+           int_path=$(echo "$previous_path" | sed "s/obsfile: /..\/rundir-${ctest::-5}\//gI")
+           new_path=$(echo "$int_path" | sed "s/solver/observer/gI")
+           obs_filename=${new_path}
            sed -i "s#@OBSFILE@#${obs_filename}#" ./$temp_yaml
-	fi 
+        fi
 
     done
 
@@ -87,23 +100,19 @@ process_obtypes() {
     sed -i "s#@DISTRIBUTION@#${distribution}#" ./$temp_yaml
 }
 
-# Loop over basic config yamls 
+# Loop over basic config yamls
 iconfig=0
-for basic_config in "${basic_configs[@]}"; do
+for basic_config in "${!ctest_configs[@]}"; do
 
   # Create the super yaml (conv.yaml)
-  conv_yaml="${ctest_names[$iconfig]}"
+  conv_yaml="${ctest_configs[$basic_config]}"
   temp_yaml="temp.yaml"
 
   rm -f $conv_yaml  # Remove any existing file
   rm -f $temp_yaml  # Remove any existing file
 
   # Concatenate all obtypes into the super yaml
-  process_obtypes "${ctest_names[$iconfig]}" "aircar_obtype_configs[@]" "data/obs/ioda_aircar_dc.nc"             "$temp_yaml"
-  #process_obtypes "${ctest_names[$iconfig]}" "aircft_obtype_configs[@]" "data/obs/ioda_aircft_dc.nc"             "$temp_yaml"
-  process_obtypes "${ctest_names[$iconfig]}" "msonet_obtype_configs[@]" "data/obs/ioda_msonet_dc.nc"             "$temp_yaml"
-  process_obtypes "${ctest_names[$iconfig]}" "atms_obtype_configs[@]"   "data/obs/atms_npp_obs_2024052700_dc.nc" "$temp_yaml"
-  #process_obtypes "${ctest_names[$iconfig]}" "amsua_obtype_configs[@]"  "data/obs/ioda_amsua_n19_dc.nc"      "$temp_yaml"
+  process_obtypes obtype_configs keys_obtype "${conv_yaml}" "$temp_yaml"
 
   # Copy the basic configuration yaml into the super yaml
   cp -p templates/basic_config/$basic_config ./$conv_yaml
@@ -117,9 +126,8 @@ for basic_config in "${basic_configs[@]}"; do
   # Replace the @OBSFILE@ placeholder with a dummy filename (can customize as needed)
   sed -i "s#@OBSFILE@#data/obs/combined_obs_file.nc#" ./$conv_yaml
 
-  echo "Super YAML created in ${conv_yaml}"
-
   # Move to testinput and remove the old temporary yaml
+  echo "Super YAML created in ${conv_yaml}"
   mv $conv_yaml ../testinput/$conv_yaml
   rm -f $temp_yaml
 
