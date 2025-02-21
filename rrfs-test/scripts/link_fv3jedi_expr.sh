c711c510746b955bdb74bfe6a11c12846b380931
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-02-21 08:25:46 -0700

    use consistent fixed prescribed ioda file names at the DA runtime

--- rrfs-test/scripts/link_fv3jedi_expr.sh
+++ rrfs-test/scripts/link_fv3jedi_expr.sh
@@ -24,7 +24,21 @@ sed -e "s#@RDASApp@#${RDASApp}#" ${RDASApp}/rrfs-test/scripts/templates/fv3jedi_
 cp ${RDASApp}/rrfs-test/ush/colormap.py . 
 cp ${RDASApp}/rrfs-test/ush/fv3jedi_increment_singleob.py .
 cp ${RDASApp}/rrfs-test/ush/fv3jedi_increment_fulldom.py . 
-ln -snf ${RDASApp}/fix/expr_data/${exprname}/Data Data
+rm -rf Data; mkdir Data
+ln -snf ${RDASApp}/fix/expr_data/${exprname}/Data/* Data/
+# link correct ioda files
+rm -rf Data/obs; mkdir  Data/obs
+set -x
+ln -snf ${RDASApp}/fix/expr_data/${exprname}/Data/obs/* Data/obs/  # keep this line for now to be backward compatible
+for dcfile in Data/obs/ioda*dc.nc; do # link to DA runtime prescribed file names
+  dcfile=${dcfile##*obs/}
+  regfile="${dcfile%%_dc.nc}.nc"
+  ln -snf ${RDASApp}/fix/expr_data/${exprname}/Data/obs/${dcfile} Data/obs/${regfile}
+done
+ln -snf ${RDASApp}/fix/expr_data/${exprname}/Data/obs/amsua_n19_obs.2024052700_dc.nc Data/obs/ioda_amsua_n19.nc
+ln -snf ${RDASApp}/fix/expr_data/${exprname}/Data/obs/atms_n20_obs_2024052700_dc.nc Data/obs/ioda_atms_n20.nc
+ln -snf ${RDASApp}/fix/expr_data/${exprname}/Data/obs/atms_npp_obs_2024052700_dc.nc  Data/obs/ioda_atms_npp.nc
+#
 ln -snf ${RDASApp}/fix/expr_data/${exprname}/DataFix DataFix
 ln -snf ${RDASApp}/fix/expr_data/${exprname}/Data_static Data_static
 ln -snf ${RDASApp}/fix/expr_data/${exprname}/INPUT INPUT
