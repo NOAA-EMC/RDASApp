3dc7019c23a8ad14d45209edd8c880fecd3a78c4
SamuelDegelia-NOAA <Samuel.Degelia@noaa.gov>
2025-02-27 14:53:36 +0000

    Update test references and build script

--- build.sh
+++ build.sh
@@ -142,25 +142,24 @@ else
   exit 1
 fi
 
-# Link in test data for experiments and create ctest yamls: MPAS-JEDI
+# Link in test data for experiments: MPAS-JEDI
 if [[ $DYCORE == 'MPAS' || $DYCORE == 'FV3andMPAS' ]]; then
   # Link in case data
   echo "Linking in test data and creating ctest yamls for MPAS-JEDI case"
-  cd $dir_root/rrfs-test/validated_yamls
-  ./gen_yaml_mpasjedi_ctest.sh
-  cd ${BUILD_DIR}
   $dir_root/rrfs-test/scripts/link_mpasjedi_expr.sh
 fi
 
-# Link in test data for experiments and create ctest yamls: FV3-JEDI
+# Link in test data for experiments: FV3-JEDI
 if [[ $DYCORE == 'FV3' || $DYCORE == 'FV3andMPAS' ]]; then
   # Link in case data
   echo "Linking in test data and creating ctest yamls for FV3-JEDI case"
-  cd $dir_root/rrfs-test/validated_yamls
-  ./gen_yaml_fv3jedi_ctest.sh
-  cd ${BUILD_DIR}
   $dir_root/rrfs-test/scripts/link_fv3jedi_expr.sh
 fi
+
+# Build the ctest yamls 
+cd $dir_root/rrfs-test/validated_yamls
+./gen_yaml_ctest.sh
+cd ${BUILD_DIR}
 
 CMAKE_OPTS+=" -DMPIEXEC_MAX_NUMPROCS:STRING=120 -DBUILD_SUPER_EXE=$BUILD_SUPER_EXE"
 # Configure
