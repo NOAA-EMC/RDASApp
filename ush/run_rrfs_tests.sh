84e8db6ba4c923872cd2c1f836ffb2469787a9a2
Samuel Degelia <samuel.degelia@clogin07.cactus.wcoss2.ncep.noaa.gov>
2025-04-25 19:38:54 +0000

    Don't ask for an account anymore on wcoss2 for run_rrfs_tests.sh

--- ush/run_rrfs_tests.sh
+++ ush/run_rrfs_tests.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 #
-export SLURM_ACCOUNT=${1}
 
-if [[ "${1}" == "" ]]; then
-  echo "Usage: ${0}  account_name"
-  exit 1
+ushdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source ${ushdir}/detect_machine.sh
+if [[ "${MACHINE_ID}" != "wcoss2" ]]; then
+  export SLURM_ACCOUNT=${1}
+  if [[ "${1}" == "" ]]; then
+    echo "Usage: ${0}  account_name"
+    exit 1
+  fi
 fi
 
 #
-ushdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source ${ushdir}/load_rdas.sh
 
 set -x
