4290d9484a8c96e689aeffd3b00dbc0db4606f90
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-07-27 13:22:05 -0600

    add modulefiles for derecho

---
+++ modulefiles/RDAS/derecho.intel.lua
@@ -0,0 +1,30 @@
+help([[
+Load environment for running the RDAS application with Intel compilers and MPI.
+]])
+
+local pkgName    = myModuleName()
+local pkgVersion = myModuleVersion()
+local pkgNameVer = myModuleFullName()
+
+prepend_path("MODULEPATH", '/glade/work/epicufsrt/contrib/spack-stack/derecho/modulefiles')
+prepend_path("MODULEPATH", '/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.8.0/envs/ue-intel-2021.10.0/install/modulefiles/Core')
+
+load("stack-intel/2021.10.0")
+load("stack-python/3.11.7")
+load("stack-cray-mpich/8.1.25")
+load("jedi-mpas-env/1.0.0")
+load("jedi-fv3-env/1.0.0")
+
+setenv("CC","mpiicc")
+setenv("FC","mpiifort")
+setenv("CXX","mpiicpc")
+
+local mpiexec = '/apps/slurm/default/bin/srun'
+local mpinproc = '-n'
+setenv('MPIEXEC_EXEC', mpiexec)
+setenv('MPIEXEC_NPROC', mpinproc)
+
+whatis("Name: ".. pkgName)
+whatis("Version: ".. pkgVersion)
+whatis("Category: RDASApp")
+whatis("Description: Load all libraries needed for RDASApp")
