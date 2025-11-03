a887834a36ff4b7074fdd75bea61fb31cc87995a
SamuelDegelia-NOAA <Samuel.Degelia@noaa.gov>
2025-11-03 20:25:20 +0000

    Add new ctests to build.sh

--- build.sh
+++ build.sh
@@ -219,9 +219,11 @@ if [[ $BUILD_RRFS_TEST == 'YES' ]]; then
     # Algorithm ctests
     rrfs_fv3jedi_2024052700_3dvar.yaml
     rrfs_fv3jedi_2024052700_3denvar.yaml
+    rrfs_fv3jedi_2024052700_3denvar_mgbf.yaml
     rrfs_fv3jedi_2024052700_getkf_observer.yaml
     rrfs_fv3jedi_2024052700_getkf_solver.yaml
     rrfs_fv3jedi_2024052700_hybrid3denvar.yaml
+    rrfs_fv3jedi_2024052700_hybrid3denvar_mgbf.yaml
 #    rrfs_mpasjedi_2024052700_bumploc.yaml
 #    rrfs_mpasjedi_2024052700_3denvar.yaml
 #    rrfs_mpasjedi_2024052700_getkf_observer.yaml
