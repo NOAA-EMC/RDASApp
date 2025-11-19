c77a8d5e9828951c5ddc2c497fd5e58ff80dda51
Sam Degelia <sdegelia@hercules-login-4.hpc.msstate.edu>
2025-11-19 10:34:29 -0600

    Add Sijie's OEFPC fix

--- build.sh
+++ build.sh
@@ -298,12 +298,17 @@ if [[ $BUILD_WORKAROUND == 'YES' ]]; then
   cp ../sorc/_workaround_/fv3-jedi/fv3jedi_io_fms2_mod.f90 ../sorc/fv3-jedi/src/fv3jedi/IO/FV3Restart
 
   # Workaround for adding MGBF
+  # No PR yet
   mkdir -p ../sorc/saber/src/saber/mgbf
   cp -r ../sorc/_workaround_/saber/mgbf/mgbf_src/*    ../sorc/saber/src/saber/mgbf/
   cp ../sorc/_workaround_/saber/mgbf/Localization.h   ../sorc/saber/src/saber/oops/Localization.h
   cp ../sorc/_workaround_/saber/mgbf/Interpolation.cc ../sorc/saber/src/saber/interpolation/Interpolation.cc
   cp ../sorc/_workaround_/saber/mgbf/CMakeLists.txt   ../sorc/saber/src/saber/CMakeLists.txt
   cp ../sorc/_workaround_/saber/mgbf/compiler_flags_Intel_Fortran.cmake  ../sorc/saber/cmake/compiler_flags_Intel_Fortran.cmake
+
+  # Workaround for ObsErrorFactorPressureCheck
+  # UFO PR: https://github.com/JCSDA-internal/ufo/pull/3899
+  cp ../sorc/_workaround_/ufo/ObsErrorFactorPressureCheck.cc ../sorc/ufo/src/ufo/filters/obsfunctions/
 
 fi
 
