f569dbb4251eb65c69e80f32e10d683b4472fd76
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-11-30 18:28:36 -0700

    update derecho to use spack-stack-1.9.3

--- modulefiles/RDAS/derecho.gnu.lua
+++ modulefiles/RDAS/derecho.gnu.lua
@@ -6,13 +6,13 @@ local pkgName    = myModuleName()
 local pkgVersion = myModuleVersion()
 local pkgNameVer = myModuleFullName()
 
-prepend_path("MODULEPATH", '/glade/work/epicufsrt/contrib/spack-stack/derecho/modulefiles')
-prepend_path("MODULEPATH", '/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.8.0/envs/ue-gcc-12.2.0/install/modulefiles/Core')
+--prepend_path("MODULEPATH", '/glade/work/epicufsrt/contrib/spack-stack/derecho/modulefiles')
+prepend_path("MODULEPATH", '/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.3/envs/rebuild-ue-gcc-12.4.0/install/modulefiles/Core')
 
-load("ncarenv/23.09")
-load("stack-gcc/12.2.0")
+--load("ncarenv/23.09")
+load("stack-gcc/12.4.0")
 load("stack-python/3.11.7")
-load("stack-cray-mpich/8.1.25")
+load("stack-cray-mpich/8.1.29")
 load("jedi-mpas-env/1.0.0")
 load("jedi-fv3-env/1.0.0")
 
@@ -20,10 +20,8 @@ load("jedi-fv3-env/1.0.0")
 --setenv("FC","mpif90")
 --setenv("CXX","mpicxx")
 
-local mpiexec = '/opt/cray/pe/pals/1.2.11/bin/aprun'
-local mpinproc = '-n'
-setenv('MPIEXEC_EXEC', mpiexec)
-setenv('MPIEXEC_NPROC', mpinproc)
+setenv('MPIEXEC_EXEC', '/opt/cray/pe/pals/1.2.11/bin/aprun')
+setenv('MPIEXEC_NPROC', '-n')
 
 whatis("Name: ".. pkgName)
 whatis("Version: ".. pkgVersion)
