3d16ce1a0300ef4b72297484789e481e3c506664
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-11-30 21:39:06 -0700

    fine tuning

--- modulefiles/RDAS/derecho.gnu.lua
+++ modulefiles/RDAS/derecho.gnu.lua
@@ -6,19 +6,13 @@ local pkgName    = myModuleName()
 local pkgVersion = myModuleVersion()
 local pkgNameVer = myModuleFullName()
 
---prepend_path("MODULEPATH", '/glade/work/epicufsrt/contrib/spack-stack/derecho/modulefiles')
 prepend_path("MODULEPATH", '/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.3/envs/rebuild-ue-gcc-12.4.0/install/modulefiles/Core')
 
---load("ncarenv/23.09")
 load("stack-gcc/12.4.0")
 load("stack-python/3.11.7")
 load("stack-cray-mpich/8.1.29")
 load("jedi-mpas-env/1.0.0")
 load("jedi-fv3-env/1.0.0")
-
---setenv("CC","mpicc")
---setenv("FC","mpif90")
---setenv("CXX","mpicxx")
 
 setenv('MPIEXEC_EXEC', '/opt/cray/pe/pals/1.2.11/bin/aprun')
 setenv('MPIEXEC_NPROC', '-n')
--- modulefiles/RDAS/derecho.intel.lua
+++ modulefiles/RDAS/derecho.intel.lua
@@ -6,19 +6,13 @@ local pkgName    = myModuleName()
 local pkgVersion = myModuleVersion()
 local pkgNameVer = myModuleFullName()
 
---prepend_path("MODULEPATH", '/glade/work/epicufsrt/contrib/spack-stack/derecho/modulefiles')
 prepend_path("MODULEPATH", '/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.3/envs/rebuild-ue-oneapi-2024.2.1/install/modulefiles/Core')
 
---load("ncarenv/23.09")
 load("stack-oneapi/2024.2.1")
 load("stack-python/3.11.7")
 load("stack-cray-mpich/8.1.29")
 load("jedi-mpas-env/1.0.0")
 load("jedi-fv3-env/1.0.0")
-
---setenv("CC","mpiicc")
---setenv("FC","mpiifort")
---setenv("CXX","mpiicpc")
 
 setenv('MPIEXEC_EXEC', '/opt/cray/pe/pals/1.2.11/bin/aprun')
 setenv('MPIEXEC_NPROC', '-n')
